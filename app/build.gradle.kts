5ff45c3abcf85b0f525349a722a7108a7c482816
sjshb57 <1290848232a@gmail.com>
2025-06-19 11:51:11 +0800

    更新版本'2.0.1'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val versionMajor = 2
 val versionMinor = 0
-val versionPatch = 0
+val versionPatch = 1
 
 android {
     namespace = "top.easelink.lcg"
@@ -44,8 +44,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-        //    isShrinkResources = true
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("release")
         }
@@ -87,19 +87,65 @@ android {
 }
 
 dependencies {
+    // Room
     kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.base)
     implementation(project(":framework"))
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
+    // Debug
     debugImplementation(libs.debug.db)
+
+    // Shiply
     implementation(libs.shiply.upgrade)
     implementation(libs.shiply.upgrade.ui)
-    implementation(libs.androidx.annotation)
 
+    // AndroidX
+    implementation(libs.androidx.annotation)
     implementation(libs.google.material)
     implementation(libs.androidx.appcompat)
     implementation(libs.fragment.ktx)
     implementation(libs.androidx.tracing)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    // UI
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.vectordrawable)
+    implementation(libs.androidx.vectordrawable.animated)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.common.java8)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // WorkManager
+    implementation(libs.androidx.work.runtime)
+
+    // Third-party
+    implementation(libs.coil)
+    implementation(libs.easypermissions)
+    implementation(libs.eventbus)
+    implementation(libs.gson)
+    implementation(libs.guava)
+    implementation(libs.jsoup)
+    implementation(libs.lottie)
+    implementation(libs.multitype)
+    implementation(libs.okhttp)
+    implementation(libs.persistentcookiejar)
+    implementation(libs.photoview)
+    implementation(libs.richtext)
+    implementation(libs.shimmerlayout)
+    implementation(libs.timber)
 }
