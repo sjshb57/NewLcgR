0bc23b5d52c692201ce9cd552e185bb3d1ea7145
sjshb57 <1290848232a@gmail.com>
2025-07-25 01:32:57 +0800

    重构CacheCleanerTask

--- app/src/main/java/top/easelink/lcg/appinit/CacheCleanerTask.kt
+++ app/src/main/java/top/easelink/lcg/appinit/CacheCleanerTask.kt
@@ -1,16 +1,18 @@
 package top.easelink.lcg.appinit
 
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import top.easelink.framework.threadpool.BackGroundPool
 import top.easelink.lcg.cache.HotTopicCacheManager
 import top.easelink.lcg.cache.PreviewCacheManager
 
 object CacheCleanerTask {
 
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     fun clearCachesIfNeeded() {
-        GlobalScope.launch(BackGroundPool) {
+        scope.launch {
             delay(5000)
             PreviewCacheManager.clearAllCaches()
             HotTopicCacheManager.clearAllCaches()
